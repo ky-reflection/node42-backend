c2bc4a95ecca71e90b474b1e3de513b2ea5d4852
ky-reflection <1643705366@qq.com>
2024-05-17 00:33:49 +0800

    deploy test

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
-sudo apt-get update
-sudo apt-get install python3-dev default-libmysqlclient-dev build-essential
-pip install -r requirements.txt
 uwsgi --stop /var/www/script/uwsgi.pid
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-
 uwsgi --ini uwsgi.ini
